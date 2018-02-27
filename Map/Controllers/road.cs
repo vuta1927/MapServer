71c142334f26c34d4810b947f706c9b3914efb7a
vuta1927 <35493980+vuta1927@users.noreply.github.com>
2018-02-27 17:42:14 +0700

    up

--- Map/Controllers/road.cs
+++ Map/Controllers/road.cs
@@ -11,9 +11,9 @@ namespace Map.Controllers
         public Coordinate[] paths { get; set; }
 
         public string color { get; set; }
-        public string metaData { get; set; }
+        public MetaData metaData { get; set; }
         public double distance { get; set; }
-        public road(int id, Coordinate[] paths, double dist, string color, string metaData)
+        public road(int id, Coordinate[] paths, double dist, string color, MetaData metaData)
         {
             this.id = id;
             this.paths = paths;
@@ -31,6 +31,18 @@ namespace Map.Controllers
         {
             this.lat = lat;
             this.lng = lng;
+        }
+    }
+
+    public class MetaData
+    {
+        public string name { get; set; }
+        public string direction { get; set; }
+        public MetaData() { }
+        public MetaData(string pname, string pdirection)
+        {
+            this.name = pname;
+            this.direction = pdirection;
         }
     }
 }
